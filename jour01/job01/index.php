153141bfe8e24d77aa0cac0e8cc2b5e4c4d56cdf
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-01-02 12:01:58 +0100

    ajout du jour 01

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+$val = 6;
+$myBool = true;
+echo "<h1> $str2 $str $str3 </h1>"; 
+echo "Le resultat de l'addition est :". $val + 4 . " euros";
+echo "<br>";
+echo "<br>";
+var_dump($myBool) ;
+echo "<br>";
+echo "<br>";
+$myBool = false;
+var_dump($myBool) ;
+
+?>
