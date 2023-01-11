3c7f7d8dc1a1d166f7911e1fe797e273bda0fdba
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-01-11 18:47:35 +0100

    maj job01 et02

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -2,25 +2,25 @@
 
 session_start();
 
-
 if (!isset($_SESSION['nbvisites'])) {
-    
-    $_SESSION['nbvisites']= 0;
+    $_SESSION['nbvisites']= 1;
     echo "<h1> Le nombre de visiteurs est de : "  . $_SESSION['nbvisites'] . "</h1>";
     
 } else {
     $_SESSION['nbvisites']++;
     echo "<h1> Le nombre de visiteurs est de : "  . $_SESSION['nbvisites'] . "</h1>";
-
+    
 }
 
-if (isset($_GET['reset'])) {
-    unset($_SESSION['nbvisites']); 
-         
+if (isset($_POST['reset'])) {
+  
+        unset($_SESSION['nbvisites']);
+        session_destroy();
+        header("Refresh:0");
 }
 
 ?>
-<form action="index.php" method="get">
-    <button type="submit" name="reset" >reset</button>
+<form action="index.php" method="post">
+    <button type="submit" name="reset">reset</button>
 
 </form>
