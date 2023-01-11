5e67023ab660798f9b8c5138a4a6763a5f51b5bd
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-01-11 18:48:20 +0100

    maj job01 02

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -1,34 +1,29 @@
 <?php
 
-
-
-setcookie("nbvisites", $_COOKIE["nbvisites"]+1 , time()+36000);
-
-
-
 if (!isset($_COOKIE["nbvisites"])) {
     
-    $_COOKIE["nbvisites"]= 0;
-    
+    $_COOKIE["nbvisites"]= 1;
+    setcookie("nbvisites", 1 , time()+36000);
+    var_dump($_COOKIE["nbvisites"]);
     echo "<h1> Le nombre de visiteurs est de : "  . $_COOKIE["nbvisites"] . "</h1>";
     
 } else {
     $_COOKIE["nbvisites"]++;
+    setcookie("nbvisites", $_COOKIE["nbvisites"] , time()+36000);
     
     echo "<h1> Le nombre de visiteurs est de : "  . $_COOKIE["nbvisites"] . "</h1>";
 
 }
 
-
-if (isset($_GET['reset'])) {
+if (isset($_POST['reset'])) {
 
     setcookie("nbvisites", "", time()-36000);      
-    unset($_GET['reset']);  
-       
+    unset($_POST['reset']);  
+    header("Refresh:0");  
 }
 
 ?>
-<form action="index.php" method="get">
+<form action="index.php" method="POST">
     <button type="submit" name="reset" >reset</button>
 
 </form>
